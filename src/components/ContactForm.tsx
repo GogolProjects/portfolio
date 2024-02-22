52d870990358332d04cd457cc7ca6dfa45c0c57a
GogolProjects <agagog97@gmail.com>
2024-02-22 18:45:16 +0100

    form-functionality_2

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { FormEvent, useState } from 'react'
 import '@/style/globals.css'
 
 function ContactForm() {
@@ -7,18 +7,25 @@ function ContactForm() {
   const[email, setEmail] = useState('')
   const[message, setMessage] = useState('')
 
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault()
+  }
   return (
     <div id='Contact' className='flex  sm:flex-row flex-col sm:space-x-[10vw] sm:ml-[18vw]'>
         <div className='bg-[#CA0241]/30  shadow-2xl w-[65vw] p-4 sm:p-6   space-y-2 rounded-md'>
-            <form className='text-blue-900 text-sm text-wrap flex flex-col space-y-3'>
+            <form 
+            onSubmit={onSubmit}
+            className='text-blue-900 text-sm text-wrap flex flex-col space-y-3'>
                 <input
                 value={email}
                 type='email' 
+                onChange={(e) => setEmail(e.target.value)}
                 className=' rounded-t-lg p-3'
                 placeholder='example@example.com' />
                
                 <textarea 
                 value={message}
+                onChange={(e) => setMessage(e.target.value)}
                 placeholder='Please write Your message. I will contact with You as soon as possible.'
                 className='p-3 sm:w-full min-h-[30vw] w-[60vw] sm:min-h-[15vw] rounded-b-lg'/>
                
