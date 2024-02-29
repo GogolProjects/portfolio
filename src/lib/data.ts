cbce5835bc113b166b0ebbdac675321011119e78
GogolProjects <agagog97@gmail.com>
2024-02-29 19:51:37 +0100

    icons

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,11 @@
 
 import Portfolio from '@/images/Portfolio.png'
 import Travelit from '@/images/Travelit.png'
+import GithubIcon  from '@/images/GitHubIcon.png'
+import LinkedinIcon from '@/images/LinkedinIcon.png'
+import TravelitLogo from '@/images/TravelitLogo.jpg'
+import Logo from '@/images/Logo.png'
+
 export const links = [
 
 
@@ -42,15 +47,48 @@ export const skils = [
 export const projects = [
    {
       title: 'Traveit',
-      description: 'This is description of my project',
+      description: 'My first full-stack projects. Traveller forum, where You can share Your travel hacks and expiriences. You can post, subscribe, coemment and more....',
       imageUrl: Travelit,
 
    },
 
    {
       title: 'Portfolio',
-      description: 'This is sescription',
+      description: 'My portfolio website, which show You who I am',
       imageUrl: Portfolio,
    }
 ] as const
 
+
+export const icons = [
+{
+   name: 'GitHub',
+   hash: 'https://github.com/GogolProjects?tab=repositories',
+   icon: GithubIcon,
+},
+{
+   name: 'Travelit',
+   hash: 'https://travelit.vercel.app/',
+   icon: TravelitLogo,
+},
+{
+   name: 'Portfolio',
+   hash: '',
+   icon: Logo,
+},
+{
+   name: 'TravelitGitHub',
+   hash: 'https://github.com/GogolProjects/travelit',
+   icon: GithubIcon,
+},
+{
+   name: 'PortfolioGitHub',
+   hash: 'https://github.com/GogolProjects/portfolio',
+   icon: GithubIcon
+},
+{
+   name: 'Linkedin',
+   hash: 'https://www.linkedin.com/in/agata-gog%C3%B3%C5%82-403976213/',
+   icon: LinkedinIcon,
+}
+] as const
