0b76e037c5be9b9c93e66c0f30d40980acc921f0
GogolProjects <agagog97@gmail.com>
2024-03-03 15:40:23 +0100

    projects_tags

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -14,8 +14,8 @@ function Project({
     githubIcon,
 }: ProjectProps) {
   return (
-    <div className=' flex flex-col sm:flex-row sm:space-x-5 items-center bg-[#1b070d8e]/30 shadow-2xl w-[65vw] p-6 space-y-2 rounded-md mb-5'>
-       <div className='flex flex-row sm:flex-col items-center justify-center space-x-3 sm:space-y-2 py-3'> 
+    <div className=' flex flex-wrap sm:flex-row sm:space-x-5 items-center  bg-[#1b070d8e]/30 shadow-2xl w-[65vw] p-6 space-y-2 rounded-md mb-5'>
+       <div className='flex flex-row sm:flex-col items-center justify-center space-x-2 sm:space-y-2 py-3'> 
         <label className='w-fit sm:w-[10vw] text-3xl sm:pl-6'>{title}</label>
         <div className='flex flex-row items-start space-x-2'>
         <a href={url}>
@@ -28,15 +28,17 @@ function Project({
       </div>
         <Image src={imageUrl} width={350} alt='projectImg' />
         <p className='text-justify pt-2'>{description}</p>
-        <ul className='flex flex-wrap justify-center space-x-2 space-y-2'>
-          {tags.map((tag, index) =>(
-            <li 
-            className='w-fit p-4 items-center rounded-2xl bg-[#692a9d]/70 hover:bg-[#692a9d]'
-            key={index}>
-              {tag}
-            </li>
-          ))}
-        </ul>
+           {tags.map((tag, index) =>(
+            <div className='flex p-1 sm:p-0'>
+              <ul >
+                <li 
+                className='w-fit p-3 items-center rounded-2xl  bg-[#692a9d]/70 hover:bg-[#8645bb]'
+                key={index}>
+                  {tag}
+                </li>
+                </ul>
+            </div>
+            ))}
     </div>
   )
 }
