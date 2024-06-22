c5dce19d774eeae469a001b47612fc4a5a544c9d
GogolProjects <agagog97@gmail.com>
2024-06-22 14:55:36 +0200

    footer correction

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='bg-[#692a9d]/20 w-full justify-between py-8 px-[10vw] flex space-x-10 sm:space-x-24 text-lg '>
-     <span className='pl-[35vw] sm:px-auto r'>2024  Designed by Agata Gogół</span>
+     <span className='pl-[35vw] sm:px-auto r'>Copyright © 2024  Designed by Agata Gogół</span>
      <a href='#Start'>
         <LucideChevronUpCircle size={32} className='hover:stroke-[#771449]'/>
      </a>
